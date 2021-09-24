df784384f78251564ba871ee139b6214afc71b79
paulokmatos <kendako361@gmail.com>
2021-09-24 17:44:58 -0300

    reduzido numero de ifs no codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 
 function meuEscopo() {
 
+
     const form = document.getElementById('form');
     const resposta = document.getElementById('resposta')
 
@@ -19,60 +20,79 @@ function meuEscopo() {
 
         validarDados(pesoVerificado, alturaConvertida)
 
+        const casosPossiveis = [
+            {
+                valor: imc <= 18.4,
+                mensagem: `Seu IMC é de ${imc} e você está abaixo do Peso ideal`,
+                classe: 'success'
+            },
+            {
+                valor: imc >= 18.5 && imc <= 24.9,
+                mensagem: `Seu IMC é de ${imc} e você está com peso Normal`,
+                classe: 'success'
+            },
+            {
+                valor: imc >= 25 && imc <= 29.9,
+                mensagem: `Seu IMC é de ${imc} e você está com Sobrepeso`,
+                classe: 'success'
+            },
+            {
+                valor: imc >= 30 && imc <= 34.9,
+                mensagem: `Seu IMC é de ${imc} e você está com Obesidade Grau 1`,
+                classe: 'success'
+            },
+            {
+                valor: imc >= 35 && imc <= 39.9,
+                mensagem: `Seu IMC é de ${imc} e você está com Obesidade Grau 2`,
+                classe: 'success'
+            },
+            {
+                valor: imc >= 40,
+                mensagem: `Seu IMC é de ${imc} e você está com Obesidade Grau 3`,
+                classe: 'success'
+            },
+            {
+                valor: imc >= 60,
+                mensagem: `Longe de mim duvidar de você, mas você tem certeza que os dados estão corretos?`,
+                classe: 'warning'
+            }
+
+        ]
+
+        casosPossiveis.forEach((caso) => {
+            if (caso.valor) {
+                return resposta.innerHTML = `<p class="${caso.classe}">${caso.mensagem}</p>`
+            }
+        })
+
     })
 
-    function resultadoIMC(imc) {
-        if (imc <= 18.4) {
-            return resposta.innerHTML = `<p class="success">Seu IMC é de ${imc} e você está abaixo do Peso ideal</p>`;
-        } else if (imc >= 18.5 && imc <= 24.9) {
-            return resposta.innerHTML = `<p class="success">Seu IMC é de ${imc} e você está no Peso ideal</p>`;
-        } else if (imc >= 25 && imc <= 29.9) {
-            return resposta.innerHTML = `<p class="success">Seu IMC é de ${imc} e você está com Sobrepeso</p>`;
-        } else if (imc >= 30 && imc <= 34.9) {
-            return resposta.innerHTML = `<p class="success">Seu IMC é de ${imc} e você está com Obesidade Grau 1</p>`;
-        } else if (imc >= 35 && imc <= 39.9) {
-            return resposta.innerHTML = `<p class="success">Seu IMC é de ${imc} e você está com Obesidade Grau 2</p>`;
-        } else if (imc >= 75) {
-            return resposta.innerHTML = `<p class="danger" style="text-align: center;">Ops! acho que você acidentalmente inseriu o peso
-        <br><strong> DA SUA MÃE</strong></p>`;
-        } else if (imc >= 40) {
-            return resposta.innerHTML = `<p class="succes">Seu IMC é de ${imc} e você está com Obesidade Grau 3</p>`;
-        }
-
-
-        // switch (imc) {
-        //     case imc <= 18.4: 
-        //        return resposta.innerHTML = `<p class="succes">Seu IMC é de ${imc} e você está abaixo do Peso ideal</p>`;
-        //     case imc >= 18.5 && imc <= 24.9: 
-        //         return resposta.innerHTML = `<p class="succes">Seu IMC é de ${imc} e você está no Peso ideal</p>`;
-        //     case imc >= 25 && imc <= 29.9: 
-        //         return resposta.innerHTML = `<p class="succes">Seu IMC é de ${imc} e você está com Sobrepeso</p>`;
-        //     case imc >= 30 && imc <= 34.9 :
-        //         return resposta.innerHTML = `<p class="succes">Seu IMC é de ${imc} e você está com Obesidade Grau 1</p>`;
-        //     case imc >= 35 && imc <= 39.9: 
-        //         return resposta.innerHTML = `<p class="succes">Seu IMC é de ${imc} e você está com Obesidade Grau 2</p>`;
-        //     case imc >= 40 : 
-        //         return resposta.innerHTML = `<p class="succes">Seu IMC é de ${imc} e você está com Obesidade Grau 1</p>`;
-
-        // }
-
-
-
-    }
-
     function validarDados(peso, altura) {
-        if (isNaN(peso) && isNaN(altura)) {
-            resposta.innerHTML = '<p class="danger">Insira um Peso e Altura Válidos</p>'
-        } else if (isNaN(peso) && !isNaN(altura)) {
-            resposta.innerHTML = '<p class="danger">Insira um Peso Válido</p>';
-        } else if (!isNaN(peso) && isNaN(altura)) {
-            resposta.innerHTML = '<p class="danger">Insira uma Altura Válida</p>';
-        } else if (!isNaN(peso) && !isNaN(altura)) {
-            resultadoIMC(imc)
-        }
+        validadores = [
+            {
+                valor: isNaN(peso) && isNaN(altura),
+                mensagem: 'Insira um Peso e Altura Válidos',
+                classe: 'danger'
+            },
+            {
+                valor: isNaN(peso) && !isNaN(altura),
+                mensagem: 'Insira um Peso Válido',
+                classe: 'danger'
+            },
+            {
+                valor: !isNaN(peso) && isNaN(altura),
+                mensagem: 'Insira uma Altura Válida',
+                classe: 'danger'
+            },
+        ]
+
+
+        validadores.forEach((validador) => {
+            if (validador.valor) {
+                resposta.innerHTML = `<p class="${validador.classe}">${validador.mensagem}</p>`
+            }
+        })
     }
-
-
 
     function calcIMC(peso, altura) {
         imc = (peso / altura ** 2).toFixed(1)
